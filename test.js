9c8437de3d9a5104c212b5b8a5879936175820bc
dergint <129293943+dergint@users.noreply.github.com>
2023-08-16 18:34:10 +0100

    update js file

--- test.js
+++ test.js
@@ -1,9 +1,18 @@
-const options = {
-	authProvider,
-};
+let athleteFinalPosition = 'first place';
 
-const client = Client.init(options);
-
-let messages = await client.api('/me/messages')
-	.select('sender,subject')
-	.get();
+switch (athleteFinalPosition) {
+  case 'first place' :
+  console.log ('You get the gold medal!');
+  break;
+  case 'second place' :
+  console.log ('You get the silver medal!');
+  break;
+  case 'third place':
+  console.log ('You get the bronz medal!');
+  break;
+  default:
+  console.log ('No medal awarded.');
+  break;
+}
+athleteFinalPosition = 'second place';
+console.log (athleteFinalPosition);
